9eb559d66c541f8b00c4c10b28b791a4e09d7d99
StasSaltykov <stasyanych92@gmail.com>
2022-03-11 23:20:21 +0200

    cw27

--- src/services/college.js
+++ src/services/college.js
@@ -1,3 +1,5 @@
+import _ from "lodash";
+
 // Data processor
 export default class College {
     #courseData
@@ -6,13 +8,17 @@ export default class College {
         this.#courses = courses;
         this.#courseData = courseData;
     }
-    addCourse(course) {
+    async addCourse(course) {
+        //if course is valid, then course should be added : this.#courses.add(course)
+        //if course is invalid, then the method returns full message describing what's wrong
+        //if course is valid
+        //converting from strings to the proper types
         course.hours = +course.hours;
         course.cost = +course.cost;
         course.openingDate = new Date(course.openingDate);
         const validationMessage = this.#getValidationMessage(course);
         if(!validationMessage) {
-           return this.#courses.add(course);
+           return await this.#courses.add(course);
         } 
         return validationMessage;
     }
@@ -32,7 +38,33 @@ export default class College {
           `wrong opening date - year should be in range [${minYear} - ${maxYear}]` : ''
          return message;
     }
-    getAllCourses() {
-        return this.#courses.get()
+    async getAllCourses() {
+        return await this.#courses.get()
+    }
+    sortCourses(key) {
+        return _.sortBy(this.getAllCourses(), key)
+    }
+    #getStatistics(interval, field) {
+        const courses = this.getAllCourses();
+        const objStat =  _.countBy(courses, e => {   
+            return Math.floor(e[field]/interval);
+         });
+         return Object.keys(objStat).map(s => {
+             return {minInterval: s * interval,
+                 maxInterval: s * interval + interval -1,
+                amount: objStat[s]}
+         })
+    }
+    getHoursStatistics(lengthInterval){
+        return this.#getStatistics(lengthInterval, 'hours');
+    }
+    getCostStatistics(lengthInterval) {
+        return this.#getStatistics(lengthInterval, 'cost')
+    }
+    removeCourse(id) {
+        if (!this.#courses.exists(id)) {
+            throw `course with id ${id} not found`
+        }
+        return this.#courses.remove(id);
     }
 }
